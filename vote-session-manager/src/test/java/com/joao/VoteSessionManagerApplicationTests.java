c53041c942cc7e82938e89256ac96a4a4e5935fa
João Vitor Costa <dev.es.costa@gmail.com>
2022-07-31 00:47:32 -0300

    - Criação do projeto base com spring boot e java 17 - Criação do esqueleto dos pacotes da arquitetura limpa - Criação da camada REST - Criação dos nomes dos casos de usos

---
+++ vote-session-manager/src/test/java/com/joao/VoteSessionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteSessionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
